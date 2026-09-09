d17088b653748f8fd08ec36c2664d85494817097
saieswarp-ios <saieswarp@icloud.com>
2026-09-09 10:53:54 +0530

    Day-2

---
+++ XpertAlertsNewV/Presentation/ChangePassword/ChangePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordView.swift
+//  XpertAlertsNewV
+//
+//  Created by IOS DEV on 08/09/26.
+//
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordView()
+}
